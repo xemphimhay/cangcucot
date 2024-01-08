c20463b277d9f1a907f685e4332ac639f2d5385e
xemphimhay <lanhp871@gmail.com>
2024-01-08 10:30:08 +0700

    Update section_thumb.blade.php

--- resources/views/themebptv/inc/section/section_thumb.blade.php
+++ resources/views/themebptv/inc/section/section_thumb.blade.php
@@ -1,7 +1,7 @@
 <section>
     <div class="Top">
         <h1>
-            <a href="{{ $item['link'] }}">{{ $item['label'] }} <i class=" fa fa-angle-right"></i></a>
+            {{ $item['label'] }} <i class=" fa fa-angle-right"></i>
         </h1>
     </div>
     <ul class="MovieList Rows AX A06 B04 C03 E20">
@@ -9,4 +9,5 @@
             @include('themes::themebptv.inc.section.section_thumb_item')
         @endforeach
     </ul>
+    <a href="{{ $item['link'] }}" class="viewall">Xem thêm.. <i class="ion-ios-arrow-right"></i></a>
 </section>
